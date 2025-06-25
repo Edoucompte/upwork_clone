8832c575cbee164ceb78c84f86bc294c091939d8
edouardo <edouardoaf2@gmail.com>
2025-06-25 11:02:07 +0100

    unify signup pages navbar

--- src/pages/SignupStep2.jsx
+++ src/pages/SignupStep2.jsx
@@ -1,3 +1,4 @@
+import SignupNavbar from '../signup/SignupNavbar'
 import React, { useState } from 'react'
 
 export default function SignupStep2() {
@@ -37,27 +38,7 @@ export default function SignupStep2() {
   return (
     <>
         {/* HEADER */}
-        <header>
-            <nav className=''>
-                <div className="navbar bg-base-100 shadow-sm">
-                    <div className="flex-none">
-                        <button className="btn btn-square btn-ghost">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
-                        </button>
-                    </div>
-                    <div className="flex-1 flex">
-                        <a className="m-auto btn btn-ghost text-xl font-bold">Upwork-clone</a>
-                    </div>
-                    {/* 
-                        <div className="flex-none">
-                            <button className="btn btn-square btn-ghost">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
-                            </button>
-                        </div>
-                    */}
-                </div> 
-            </nav>
-        </header>
+        <SignupNavbar/>
 
         {/* CONTENT */}
         <main className='mt-30 p-8 md:p-30'>
